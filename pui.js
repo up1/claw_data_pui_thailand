ed7e3cdabd9f6f42ebce774971963816a1961164
up1 <somkiat.p@gmail.com>
2020-04-06 22:54:29 +0700

    Add dockerfile

--- pui.js
+++ pui.js
@@ -4,7 +4,8 @@ const puppeteer = require('puppeteer');
 
 (async() => {
     const browser = await puppeteer.launch({
-        timeout: 30000
+        timeout: 30000,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage(); 
 
